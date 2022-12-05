3fd8af9c307865b441e6530616a539b4f97012f6
Skyhero-alt <skyhero.code@gmail.com>
2022-12-05 18:54:31 +0530

    tp

---
+++ README.md
@@ -0,0 +1,14 @@
+# FlowTime
+A java swing application that my team was working on as a part of our Java Curriculum
+
+To run the program on your system, open the project directory and type the below commands to compile and run the program 😀
+```shell
+javac WindowFrame.java
+```
+```shell
+java WindowFrame
+```
+<img src="https://cdn.discordapp.com/attachments/905074139327516691/917692544643657728/unknown.png">
+
+- One can have upto 20 pending tasks at once
+- The default timer is set to 25 mins per task, which can be changed using the `settings option`
---
+++ TitleBar.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TitleBar extends JPanel{
+	JButton settingButton = new JButton("⚙️ Setting");
+	
+	TitleBar(){
+		this.setPreferredSize(new Dimension(500,80));
+		JLabel titleText= new JLabel("FLOWTIME APP");
+		titleText.setPreferredSize(new Dimension(300,75));
+		titleText.setFont(new Font("Manjari",Font.BOLD, 30));
+		titleText.setForeground(Color.white);
+		titleText.setHorizontalAlignment(JLabel.CENTER);
+
+		this.add(titleText);
+		this.add(settingButton);
+		
+		settingButton.setBounds(100, 160, 200, 60);
+		settingButton.setBackground(new Color(139,156,225));
+		settingButton.setForeground(Color.white);
+		settingButton.setFocusable(false);
+	}
+	
+	JButton getSettingButton(){
+		return settingButton;
+	}
+}
